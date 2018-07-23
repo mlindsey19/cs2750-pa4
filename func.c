fd2d8053981c00aef93bf8f5922453b9580f032e
Mitch Lindsey <dmld54@mail.umsl.edu>
2018-07-23 10:43:44 -0500

    add func.c

---
+++ func.c
@@ -0,0 +1,9 @@
+//David Lindsey
+//cs 2750 pa5
+//22-07-18
+
+#include <stdlib.c>
+
+void sumNumbers( int *sum, const char *argv){
+	*sum +=	atoi( argv );
+}
--- main.c
+++ main.c
@@ -3,8 +3,23 @@
 //22-07-18
 
 #include <stdio.c>
+#include <stdlib.c>
 
 int main(argc, char *argv[]){
+
+	int *sum = 0;
+	
+	for ( int p = 1; p <= argc; p++ ){
+		
+		for ( int i = 0; i < strlen( argv[p] ); i++ ){	
+			if (!isdigit(argv[p][i])) {
+				break;
+			}
+			else {
+				//sumNumbs(*sum, argv[p]);
+			}
+		}
+	}
 
 
 return 0;
